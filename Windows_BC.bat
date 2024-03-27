29429da1b542dac3a19a4e48bbafbc3e1da2a3ad
Huiming Zhang <me@yxzhm.com>
2024-03-27 19:23:27 +0800

    Update Windows_BC.bat

--- Windows_BC.bat
+++ Windows_BC.bat
@@ -1,3 +1,6 @@
+%windir%\SysWOW64\reg.exe delete "HKEY_CURRENT_USER\Software\Scooter Software\Beyond Compare 5" /v CacheID /f
+%windir%\SysWOW64\reg.exe delete "HKEY_CURRENT_USER\Software\Scooter Software\Beyond Compare 5" /v Expired /f
+
 %windir%\SysWOW64\reg.exe delete "HKEY_CURRENT_USER\Software\Scooter Software\Beyond Compare 4" /v CacheID /f
 %windir%\SysWOW64\reg.exe delete "HKEY_CURRENT_USER\Software\Scooter Software\Beyond Compare 4" /v Expired /f
 
@@ -6,5 +9,6 @@
 
 %windir%\SysWOW64\reg.exe delete "HKEY_CURRENT_USER\Software\Scooter Software\Beyond Compare" /v Expired /f
 
+del "%USERPROFILE%\AppData\Roaming\Scooter Software\Beyond Compare 5\*.*" /Q
 del "%USERPROFILE%\AppData\Roaming\Scooter Software\Beyond Compare 4\*.*" /Q
 del "%USERPROFILE%\AppData\Roaming\Scooter Software\Beyond Compare 3\*.*" /Q
